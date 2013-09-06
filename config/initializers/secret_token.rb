9eb70a2d3eabaeb933c80956f6d3f71043be984d
Shane Lee <admin@innatelycurious.com>
2013-09-06 14:59:20 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shenapp::Application.config.secret_token = 'b2fb3c74c6fd52e4969ff1a7af0c4734bca38e156dbe80a44cff0a438e984b47208ee186c5e9ba9a2d59983d12ff6654e852b6bb411621beaead30ccc5e635e4'
